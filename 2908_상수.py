79e0e89a2ae9d07a8ee51e5da75ec0833016867b
Xsop-sop <songjaey08@gmail.com>
2022-01-06 20:21:54 +0900

    2908_상수

---
+++ 2908_상수.py
@@ -0,0 +1,12 @@
+string = input().split()
+lists = []
+
+for i in range(len(string)):
+    lists.append(string[i][::-1])
+
+lists = list(map(int, lists))
+
+maximum = lists[0]
+if maximum < lists[1]:
+    maximum = lists[1]
+print(maximum)
