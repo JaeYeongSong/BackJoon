0db14aa1ad3fea87f607292bb336feec91e0d0bb
Xsop-sop <songjaey08@gmail.com>
2022-01-17 10:00:09 +0900

    4948_베르트랑 공준

---
+++ 4948_베르트랑 공준.py
@@ -0,0 +1,27 @@
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+all_list = list(range(2,246912))
+memo = []
+
+for i in all_list:
+    if isPrime(i):
+        memo.append(i)
+
+n = int(input())
+
+while True:
+    count = 0
+    if n == 0 :
+        break
+    for i in memo:
+        if n < i <= 2 * n:
+            count += 1
+    print(count)
+    n = int(input())
