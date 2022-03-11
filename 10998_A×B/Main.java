30edea22f94fc1bd43d4969c6c38008ab6110138
JaeYeongSong <songjaey08@gmail.com>
2022-03-11 09:14:30 +0900

    Java - 10998_A×B

---
+++ 10998_A×B/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] number = scan.nextLine().split(" ");
+        System.out.println(Integer.parseInt(number[0]) * Integer.parseInt(number[1]));
+    }
+}
